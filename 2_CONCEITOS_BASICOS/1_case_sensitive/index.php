d0a787e38353f7c6b29659b597ed44598fe5eaff
murilosolino <murilo.solino@outlook.com>
2025-05-02 19:46:18 -0300

    PHP BASICO: Conceitos

---
+++ 2_CONCEITOS_BASICOS/1_case_sensitive/index.php
@@ -0,0 +1,16 @@
+<?php 
+
+// Nao eh case sensitive
+echo "teste <br>";
+ecHO "testando 2 <BR>";
+ECHO "testando 3 <br>";
+
+// eh case sensitive
+$nome = "Fulano";
+$NOME = "Beltrano";
+
+echo $nome;
+echo "<br>";
+echo $NOME;
+
+?>
---
+++ 2_CONCEITOS_BASICOS/2_instrucao_de_codigo/index.php
@@ -0,0 +1,9 @@
+<?php
+
+echo "Uma linha<br>"; //delimitando bloco de codigo por ponto e virgula
+echo "Uma outra linha <br>"; 
+
+    if(5>2){
+        echo "Mais uma linha";
+    } // delimitando bloco por chave
+?>
